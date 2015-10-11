a40cbd7fbf5b27155eef2c028a8c56a24d83cfe0
Guillermo Iván Jiménez Ruvalcaba <engel_live_aus_rammsteinland@hotmail.com>
2015-10-11 02:31:13 -0500

    improved order. Better results

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+var evaluateString = function evaluate(string) {
+  'use strict';
+  let states = estados();
+  let analyzer = analizador();
+  let feed = analyzer.feeder(string + ' ');
+  let currentState = states.initialState;
+  for (let symbol of feed) {
+    currentState = analyzer.automatonFeed(states.statesObjArr, currentState, symbol);
+  }
+  if (states.finalStates.indexOf(currentState) > -1) {
+    window.alert("Tu cadena es válida");
+  } else {
+    window.alert("Tu cadena es inválida");
+  }
 
+};
